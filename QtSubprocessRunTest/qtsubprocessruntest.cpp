023dcde61e117c2811143fbe11fe3bbff75e13fd
jian peng <jian.peng@ericsson.com>
2019-09-29 18:11:19 +0800

    Add widget platter set

--- QtSubprocessRunTest/qtsubprocessruntest.cpp
+++ QtSubprocessRunTest/qtsubprocessruntest.cpp
@@ -1,9 +1,21 @@
-#include "qtsubprocessruntest.h"
+﻿#include "qtsubprocessruntest.h"
 
 QtSubprocessRunTest::QtSubprocessRunTest(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	// 设置窗口背景颜色
+	QPalette pal = window()->palette();
+	pal.setColor(QPalette::Window, QRgb(0x018bba));
+	pal.setColor(QPalette::WindowText, QRgb(0x018bba));
+	pal.setColor(QPalette::ButtonText, QRgb(0x018bba));
+	pal.setColor(QPalette::Light, QRgb(0x018bba));
+	window()->setPalette(pal);
+	pal = this->ui.pushButton_Start->window()->palette();
+	pal.setColor(QPalette::Window, QRgb(0xd6d6d6));
+	pal.setColor(QPalette::WindowText, QRgb(0x018bba));
+	pal.setColor(QPalette::ButtonText, QRgb(0x018bba));
+	this->ui.pushButton_Start->window()->setPalette(pal);
 	m_pyPath = QApplication::applicationDirPath() + "/test.py";
 	connect(this->ui.pushButton_Start, &QPushButton::clicked, this, &QtSubprocessRunTest::startRunPyScript);
 	connect(this->ui.pushButton_Stop, &QPushButton::clicked, this, &QtSubprocessRunTest::stopRunPyScript);
@@ -28,7 +40,7 @@ void QtSubprocessRunTest::stopRunPyScript()
 void QtSubprocessRunTest::displayPyStdoutToBorwse(const QString &stdoutcontents)
 {
 	QString stdoutcontent = stdoutcontents;
-	stdoutcontent = stdoutcontent.replace(QRegExp("\r\n\r\n"),"\r\n");
+	stdoutcontent = stdoutcontent.replace(QRegExp(".*(\r\n\r\n)"),"\r\n");
 	this->ui.textBrowser->append(stdoutcontent);
 	this->ui.textBrowser->update();
 }
